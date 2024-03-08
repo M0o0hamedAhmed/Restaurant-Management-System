3dc4f0985a49a34911da298b0836d12d5651fd74
M0o0hamedAhmed <mabolaumon@gmail.com>
2024-03-08 11:01:51 +0200

    adding preventLazyLoading to App Service Provider for prevent N+1 Query Problems

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
@@ -30,6 +31,7 @@ class AppServiceProvider extends ServiceProvider
                 ->uncompromised();
         });
 
+        Model::preventLazyLoading(! app()->isProduction());
         Paginator::useBootstrap();
     }
 }
